a100305012976d4396f9181fa64ac4fc1eadb962
Nick Jones <nickjones9605@gmail.com>
2022-07-07 21:35:28 -0600

    adds validates presence of for name, number of lakes, biome, and land area

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,8 @@
-class State < ApplicationRecord
-    
+class State < ActiveRecord::Base 
+    has_many :cities
+
+    validates_presence_of :name
+    validates_presence_of :number_of_lakes
+    validates_presence_of :biome
+    validates_presence_of :land_area
 end
