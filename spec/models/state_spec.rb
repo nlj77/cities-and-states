097327da9979fca99658b6a624b8bd3dbbb66b48
Nick Jones <nickjones9605@gmail.com>
2022-07-07 21:05:09 -0600

    updates state spec to have many cities

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'  
 
-RSpec.describe State do
-    it {should have_many :cities}
+RSpec.describe State, type: :model do
+    describe 'relationships' do
+        it {should have_many :cities }
+    end
+
+    describe 'validations' do
+        it { should validate_presence_of :name }
+    end
 end
