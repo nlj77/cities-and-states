d53fa7eb76cc4ccbe3e393edb25e1fd2bec93fac
Nick Jones <nickjones9605@gmail.com>
2022-07-07 21:04:38 -0600

    create city spec.rb

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper'
+
+RSpec.describe City, type: :model do
+    describe 'relationships' do
+        it {should belong_to :state}
+    end
+
+    describe 'validations' do
+        it { should validate_presence_of :population }
+        it { should validate_presence_of :capitol }
+        it { should validate_presence_of :name}
+    end
